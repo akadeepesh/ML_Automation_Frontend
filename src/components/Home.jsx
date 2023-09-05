97b4363a406a3c2542e05f5e2c4976f06f6cb9d8
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-09-05 17:08:40 +0530

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
 import './styling/Home.css';
+import { BsArrowRightCircle, BsFillInfoCircleFill } from "react-icons/bs";
 
 const Home = () => {
   return (
     <div className='max-w-screen h-[1000px]'>
-      <div className="box w-72 h-44 bg-purple-600 rounded-3xl m-10">
-        <span className='m-4 text-white main text-2xl'>Text Tokenization</span>
+      <div className="TEXT m-12 flex mt-12 space-x-40">
+        <div className="BOX w-72 h-44 flex rounded-3xl">
+          <div className='m-2 ml-4 text-white MAIN text-2xl'>
+            Text Tokenization
+          </div>
+          <div className="grid ml-6 mt-24">
+            <span className='grid-flow-col cursor-pointer text-white'>
+              <BsFillInfoCircleFill size={30} className='' />
+            </span>
+            <span className='grid-flow-col cursor-pointer text-white hover:text-black'>
+              <BsArrowRightCircle size={30} className='' />
+            </span>
+          </div>
+        </div>
+        <div className="BOX w-72 h-44 flex rounded-3xl">
+          <div className='m-2 ml-4 text-white MAIN text-2xl'>
+            Text Tokenization
+          </div>
+          <div className="grid ml-6 mt-24">
+            <span className='grid-flow-col cursor-pointer text-white'>
+              <BsFillInfoCircleFill size={30} className='' />
+            </span>
+            <span className='grid-flow-col cursor-pointer text-white hover:text-black'>
+              <BsArrowRightCircle size={30} className='' />
+            </span>
+          </div>
+        </div>
+        <div className="BOX w-72 h-44 flex rounded-3xl">
+          <div className='m-2 ml-4 text-white MAIN text-2xl'>
+            Text Tokenization
+          </div>
+          <div className="grid ml-6 mt-24">
+            <span className='grid-flow-col cursor-pointer text-white'>
+              <BsFillInfoCircleFill size={30} className='' />
+            </span>
+            <span className='grid-flow-col cursor-pointer text-white hover:text-black'>
+              <BsArrowRightCircle size={30} className='' />
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react'; //{ useState } 
 import { Link } from 'react-scroll';
 import './styling/Navbar.css'
 
@@ -7,30 +7,30 @@ const Navbar = () => {
     return (
         <header>
             <nav>
-                <div class="bar">
-                    <div class="flex justify-between h-16 px-10 shadow items-center">
-                        <div class="flex items-center space-x-8">
-                            <div class="text-xl text-brownish lg:text-2xl font-bold cursor-pointer">
+                <div className="bar">
+                    <div className="flex justify-between h-16 px-10 shadow items-center">
+                        <div className="flex items-center space-x-8">
+                            <div className="text-xl text-brownish lg:text-2xl font-bold cursor-pointer">
                                 <a href="/">DeCoder</a>
                             </div>
-                            <ul class="hidden md:flex justify-around cursor-pointer space-x-20 transition ease-in-out">
-                                <li className='hover:text-indigo-600 ml-80 text-brownish'>
+                            <ul className="md:flex justify-around cursor-pointer space-x-20">
+                                <li className='hover:text-indigo-600 ml-80 text-brownish transition-all ease-in-out'>
                                     <Link to='home' smooth duration={500}>Home</Link>
                                 </li>
                                 <li className='hover:text-indigo-600 text-brownish'>
                                     <Link to='about' smooth duration={500}>About</Link>
                                 </li>
                                 <li className='hover:text-indigo-600 text-brownish'>
-                                    <Link to='service' smooth duration={500}>Service</Link>
+                                    <Link to='contact' smooth duration={500}>Contact</Link>
                                 </li>
                                 <li className='hover:text-indigo-600 text-brownish'>
-                                    <Link to='contact' smooth duration={500}>Contact</Link>
+                                    <Link to='feedback' smooth duration={500}>Feedback</Link>
                                 </li>
                             </ul>
                         </div>
-                        <div class="flex space-x-4 items-center">
-                            <a href="#" class="text-brownish text-sm">LOGIN</a>
-                            <a href="#" class="bg-indigo-600 px-4 py-2 rounded-2xl text-white hover:bg-indigo-500 text-sm">SIGN UP</a>
+                        <div className="flex space-x-4 items-center">
+                            <a href="/" className="text-brownish text-sm">LOGIN</a>
+                            <a href="/" className="bg-indigo-600 px-4 py-2 rounded-2xl text-white hover:bg-indigo-500 text-sm">SIGN UP</a>
                         </div>
                     </div>
                 </div>
