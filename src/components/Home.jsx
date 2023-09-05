5f593843ab1fb1d85df223437eb8b2799a55fe20
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-09-06 02:50:10 +0530

    bert

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,13 @@ import { BsArrowRightCircle, BsFillInfoCircleFill } from "react-icons/bs";
 const data = [
   { title: 'Text Tokenization', description: 'Text tokenization is the process of breaking down a piece of text into smaller units called tokens. These tokens can be words, characters, or subwords.' },
   { title: 'Stop Word Removal', description: 'Stop word removal is the process of removing commonly used words, such as “the”, “a”, “an”, and “in”, from a text.' },
-  { title: 'Stemming / Lemmatization', description: 'Stemming and lemmatization are both techniques used in natural language processing (NLP) to reduce words to their base or root form.' },
+  { title: 'Stemming/ Lemmatization', description: 'Stemming and lemmatization are both techniques used in natural language processing (NLP) to reduce words to their base or root form.' },
 ];
+
+const bertData = {
+  title: 'BERT fine-tuning',
+  description: 'BERT fine-tuning is the process of training a pre-trained BERT model on a specific task, such as text classification or named entity recognition, to improve its performance on that task.',
+};
 
 const Home = () => {
   return (
@@ -14,12 +19,12 @@ const Home = () => {
       <div className="flex flex-wrap -mx-4 mt-20">
         {data.map((item, index) => (
           <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
-            <div className='bg-slate-600 rounded-3xl p-4 h-full flex flex-col justify-between'>
+            <div className='bg-slate-600 rounded-3xl p-4 h-full BOX flex flex-col justify-between'>
               <div>
-                <div className='text-white text-2xl mb-2'>{item.title}</div>
+                <div className='text-white text-2xl mb-4'>{item.title}</div>
                 <p className="text-white">{item.description}</p>
               </div>
-              <div className="mt-4 flex justify-between items-center">
+              <div className="mt-4 z-10 flex justify-between items-center">
                 <span className='cursor-pointer text-white hover:text-indigo-600'>
                   <BsFillInfoCircleFill size={30} />
                 </span>
@@ -32,6 +37,24 @@ const Home = () => {
         ))}
       </div>
       <hr className="my-8" />
+      <div className="flex flex-wrap -mx-4 mt-20">
+        <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 px-4 mb-8">
+          <div className='bg-slate-600 rounded-3xl p-4 h-full BOX flex flex-col justify-between'>
+            <div>
+              <div className='text-white text-2xl mb-4'>{bertData.title}</div>
+              <p className="text-white">{bertData.description}</p>
+            </div>
+            <div className="mt-4 z-10 flex justify-between items-center">
+              <span className='cursor-pointer text-white hover:text-indigo-600'>
+                <BsFillInfoCircleFill size={30} />
+              </span>
+              <span className='cursor-pointer text-white hover:text-indigo-600'>
+                <BsArrowRightCircle size={30} />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
