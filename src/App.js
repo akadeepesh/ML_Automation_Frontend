00937a069839c1fe7d4083b04fc983536f5973a7
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-09-07 05:38:55 +0530

    login

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Home from './components/Home';
 import Chatbox from './components/Chatbox';
 import Navbar from './components/Navbar';
 import Signup from './components/Signup';
+import Login from './components/Login';
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 const Layout = () => {
   const location = useLocation();
-  const isSignupPage = location.pathname === "/signup";
+  const isHidden = location.pathname === "/signup" || location.pathname === "/login";
   return (
     <>
-      {!isSignupPage && <Navbar />}
-      {!isSignupPage && <Chatbox />}
+      {!isHidden && <Navbar />}
+      {!isHidden && <Chatbox />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './styling/Signup.css';
 
-function App() {
+function Login() {
     return (
         <div className="background-radial-gradient overflow-hidden">
             <div className="container mx-auto p-4">
                 <div className="flex flex-col md:flex-row">
                     <div className="md:w-1/2 text-center md:text-md-start md:flex md:flex-col justify-center">
                         <h1 className="my-5 text-3xl font-bold leading-tight text-indigo-50">
-                            One step to <br />
-                            <span className="text-indigo-100">Dataset Creation</span>
+                            Welcome back to <br />
+                            <span className="text-indigo-100">Your App</span>
                         </h1>
 
                         <p className="text-indigo-200">
@@ -26,45 +26,29 @@ function App() {
 
                         <div className="my-5 bg-opacity-75 bg-white rounded-lg shadow-xl bg-glass">
                             <div className="p-5">
-                                <div className="grid grid-cols-2 gap-4">
-                                    <div>
-                                        <label className="block mb-4 text-gray-700">First name</label>
-                                        <input
-                                            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-indigo-500"
-                                            type="text"
-                                        />
-                                    </div>
-
-                                    <div>
-                                        <label className="block mb-4 text-gray-700">Last name</label>
-                                        <input
-                                            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-indigo-500"
-                                            type="text"
-                                        />
-                                    </div>
-                                </div>
-
                                 <label className="block mb-4 text-gray-700">Email</label>
                                 <input
                                     className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-indigo-500"
                                     type="email"
+                                    placeholder="Email"
                                 />
 
                                 <label className="block mb-4 text-gray-700">Password</label>
                                 <input
                                     className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-indigo-500"
                                     type="password"
+                                    placeholder="Password"
                                 />
 
                                 <button
                                     className="w-full py-2 mt-10 px-4 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
                                     type="button"
                                 >
-                                    Sign Up
+                                    Log In
                                 </button>
 
                                 <div className="text-center mt-4">
-                                    <p>Or sign up with:</p>
+                                    <p>Or log in with:</p>
 
                                     <div className="flex justify-center mt-2">
                                         <button className="mx-3 text-indigo-500 hover:text-indigo-600">
@@ -81,8 +65,9 @@ function App() {
                                         </button>
                                     </div>
                                 </div>
+
                                 <p className="mt-4 text-gray-700 text-center">
-                                    Already have an account? <a href="/login" className="text-indigo-500">Log in here</a>.
+                                    Don't have an account? <a href="/signup" className="text-indigo-500">Sign up here</a>.
                                 </p>
                             </div>
                         </div>
@@ -93,4 +78,4 @@ function App() {
     );
 }
 
-export default App;
+export default Login;
