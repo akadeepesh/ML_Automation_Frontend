6434efa74976e621e49d0bfe6feaaf84aea00f9c
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-11-04 19:26:33 +0530

    removed redux

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './styling/Home.css';
 import { BsArrowRightCircle, BsFillInfoCircleFill } from "react-icons/bs";
 import { Link as RouteLink } from 'react-router-dom';
-import * as HoverCard from '@radix-ui/react-hover-card';
 
 const data = [
   { title: 'Text Tokenization', description: 'Text tokenization is the process of breaking down a piece of text into smaller units called tokens. These tokens can be words, characters, or subwords.' },
@@ -28,51 +27,7 @@ const Home = () => {
   ];
   return (
     <div className='max-w-screen-xl mx-auto p-4'>
-      <HoverCard.Root>
-        <HoverCard.Trigger asChild>
-          <span
-            className="inline-block cursor-pointer rounded-full shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] outline-none focus:shadow-[0_0_0_2px_white]"
-          >
-            <BsFillInfoCircleFill size={30} />
-          </span>
-        </HoverCard.Trigger>
-        <HoverCard.Portal>
-          <HoverCard.Content
-            className="data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[300px] rounded-md bg-white p-5 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all"
-            sideOffset={5}
-          >
-            <div className="flex flex-col gap-[7px]">
-              <img
-                className="block h-[60px] w-[60px] rounded-full"
-                src="https://pbs.twimg.com/profile_images/1337055608613253126/r_eiMp2H_400x400.png"
-                alt="Radix UI"
-              />
-              <div className="flex flex-col gap-[15px]">
-                <div>
-                  <div className="text-mauve12 m-0 text-[15px] font-medium leading-[1.5]">Radix</div>
-                  <div className="text-mauve10 m-0 text-[15px] leading-[1.5]">@radix_ui</div>
-                </div>
-                <div className="text-mauve12 m-0 text-[15px] leading-[1.5]">
-                  Components, icons, colors, and templates for building high-quality, accessible UI.
-                  Free and open-source.
-                </div>
-                <div className="flex gap-[15px]">
-                  <div className="flex gap-[5px]">
-                    <div className="text-mauve12 m-0 text-[15px] font-medium leading-[1.5]">0</div>{' '}
-                    <div className="text-mauve10 m-0 text-[15px] leading-[1.5]">Following</div>
-                  </div>
-                  <div className="flex gap-[5px]">
-                    <div className="text-mauve12 m-0 text-[15px] font-medium leading-[1.5]">2,900</div>{' '}
-                    <div className="text-mauve10 m-0 text-[15px] leading-[1.5]">Followers</div>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <HoverCard.Arrow className="fill-white" />
-          </HoverCard.Content>
-        </HoverCard.Portal>
-      </HoverCard.Root>
       <div className="flex flex-wrap -mx-4 mt-20">
         {data.map((item, index) => (
           <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
