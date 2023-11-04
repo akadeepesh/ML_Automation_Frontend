76ee4bd246e9422002a4eae7b039e17fa598bbf1
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-11-04 17:33:31 +0530

    radix animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const { mauve } = require('@radix-ui/colors');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,6 +7,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        ...mauve,
         blackShade: "#33435E",
         blackish: "#0F1618",
         greenish: "#28A17D",
@@ -13,6 +15,30 @@ module.exports = {
         blueish : "#79E6F3",
         // #566BC8
         khatta: "#313D37"
+      },
+      keyframes: {
+        slideUpAndFade: {
+          '0%': { opacity: 0, transform: 'translateY(2px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideRightAndFade: {
+          '0%': { opacity: 0, transform: 'translateX(-2px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+        slideDownAndFade: {
+          '0%': { opacity: 0, transform: 'translateY(-2px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideLeftAndFade: {
+          '0%': { opacity: 0, transform: 'translateX(2px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        slideUpAndFade: 'slideUpAndFade 300ms cubic-bezier(0.16, 0, 0.13, 1)',
+        slideDownAndFade: 'slideDownAndFade 300ms cubic-bezier(0.16, 0, 0.13, 1)',
+        slideRightAndFade: 'slideRightAndFade 300ms cubic-bezier(0.16, 0, 0.13, 1)',
+        slideLeftAndFade: 'slideLeftAndFade 300ms cubic-bezier(0.16, 0, 0.13, 1)',
       },
     },
   },
