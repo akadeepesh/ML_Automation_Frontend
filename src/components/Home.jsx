c20614d09ea57d630c79813be00fcf9fa6d382e5
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-01-12 01:53:46 +0530

    home page responsiveness fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -57,8 +57,8 @@ const Home = () => {
       <div className="max-w-screen-xl mx-auto">
         <div className="flex flex-wrap mt-20">
           {data.map((item, index) => (
-            <div key={index} className="">
-              <div className="mr-10 my-10 min-h-[515.86px] rounded-lg overflow-hidden border border-neutral-200/60 bg-white text-neutral-700 shadow-sm w-[380px]">
+            <div key={index} className="w-full md:w-1/2 xl:w-1/3">
+              <div className="m-2 min-h-[515.86px] rounded-lg overflow-hidden border border-neutral-200/60 bg-white text-neutral-700 shadow-sm">
                 <div className="relative min-h-[213px]">
                   <img
                     src={item.Imagelink}
@@ -82,46 +82,31 @@ const Home = () => {
           ))}
         </div>
         <hr className="my-8 h-[2px] bg-white" />
-        <div className="flex items-center max-w-4xl overflow-hidden bg-white border rounded-lg shadow-sm border-neutral-200/60">
+        <div className="flex flex-col md:flex-row items-center max-w-4xl overflow-hidden bg-white border rounded-lg shadow-sm border-neutral-200/60">
           <img
             src="https://images.ctfassets.net/k42vp4yl2q3q/2FFU53ah4JKq9wz4y4mcJj/5325b9fb40612eb6352a0e525c3d28b0/pre-training_fine-tuning_in-context_learning.jpg?w=1920&h=1080&fl=progressive&q=50&fm=jpg"
             alt="Fine tuining"
-            className="object-cover w-auto h-[232px] aspect-video"
+            className="object-cover w-full md:w-auto h-[232px] aspect-video"
           />
           <div className="p-8">
-            <a href="#_" className="block mb-3">
+            <span href="#_" className="block mb-3">
               <h5 className="text-xl font-bold leading-none tracking-tight text-neutral-900">
                 {bertData.title}
               </h5>
-            </a>
+            </span>
             <p className="mb-4 text-sm text-neutral-500">
               {bertData.description}
             </p>
-            <button className="inline-flex items-center justify-between w-auto h-10 px-4 py-2 text-sm font-medium text-white transition-colors rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none bg-neutral-950 hover:bg-neutral-950/90">
+            <button className="inline-flex items-center justify-between w-full sm:w-auto h-10 px-4 py-2 text-sm font-medium text-white transition-colors rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none bg-neutral-950 hover:bg-neutral-950/90">
               <span>Let's go!</span>
-              <svg
-                className="w-4 h-4 ml-2 -mr-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
             </button>
           </div>
         </div>
         <hr className="my-8 h-[2px] bg-white" />
         <div className="flex flex-wrap -mx-4 mt-20">
           {Audio.map((item, index) => (
-            <div
-              key={index}
-              className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 px-4 mb-8"
-            >
-              <div className="rounded-lg overflow-hidden border border-neutral-200/60 bg-white text-neutral-700 shadow-sm w-[380px]">
+            <div key={index} className="w-full md:w-1/2 xl:w-1/3 px-4 mb-8">
+              <div className="rounded-lg overflow-hidden border border-neutral-200/60 bg-white text-neutral-700 shadow-sm">
                 <div className="relative">
                   <img
                     src={item.Imagelink}
@@ -145,6 +130,7 @@ const Home = () => {
           ))}
         </div>
       </div>
+      ;
     </>
   );
 };
