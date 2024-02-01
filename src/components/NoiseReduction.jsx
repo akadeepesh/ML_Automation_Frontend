ffbd875f8666f103c0965befda224985ffbc2a4f
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-02-02 04:13:49 +0530

    Added Noise File upload input condional rendering and design have been added too

--- src/components/NoiseReduction.jsx
+++ src/components/NoiseReduction.jsx
@@ -1,14 +1,22 @@
 import React, { useRef, useState } from "react";
+import { Checkbox } from "@material-tailwind/react";
 // import axios from "axios";
 const NoiseReduction = () => {
   const [audioFile, setAudioFile] = useState(null);
-  const [uploaded, setUploaded] = useState(false);
-  // const [noisefile, setNoiseFile] = useState(true);
+  const [noiseFile, setNoiseFile] = useState(null);
+  const [uploadedNoise, setUploadedNoise] = useState(false);
+  const [uploadedAudio, setUploadedAudio] = useState(false);
+  const [isSameFile, setIsSameFile] = useState(false);
   const audioRef = useRef(null);
+  const noiseRef = useRef(null);
 
   const handleAudioChange = (event) => {
-    setUploaded(true);
+    setUploadedAudio(true);
     setAudioFile(event.target.files[0]);
+  };
+  const handleNoiseChange = (event) => {
+    setUploadedNoise(true);
+    setNoiseFile(event.target.files[0]);
   };
 
   // const handleFileChange = async (e) => {
@@ -35,97 +43,127 @@ const NoiseReduction = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="flex flex-row w-5/6 bg-white rounded space-y-6 shadow p-6 m-4">
-        <div className="flex items-center justify-center w-1/2">
-          <label
-            htmlFor="dropzone-file"
-            className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+      <div className="flex flex-col w-5/6 bg-white rounded space-y-6 shadow p-6 m-4">
+        <Checkbox
+          className="relative"
+          label="Noise File Same as Audio File"
+          ripple={true}
+          checked={isSameFile}
+          onClick={() => setIsSameFile(!isSameFile)}
+        />
+        <div className="flex flex-row w-full space-x-12">
+          <div
+            className={`flex items-center justify-center ${
+              isSameFile ? "w-full" : "w-1/2"
+            }`}
           >
-            <div className="flex flex-col items-center justify-center pt-5 pb-6">
-              <svg
-                className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 16"
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+            >
+              <p className="mb-2 text-xl font-bold text-gray-500">Audio</p>
+              {!uploadedAudio && (
+                <>
+                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg
+                      className="w-8 h-8 mb-4 text-gray-500"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 16"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                      />
+                    </svg>
+                    <p className="mb-2 text-sm text-gray-500">
+                      <span className="font-semibold">Click to upload</span> or
+                      drag and drop
+                    </p>
+                    <p className="text-xs text-gray-500">
+                      Choose any audio file.
+                    </p>
+                  </div>
+                  <input
+                    id="dropzone-file"
+                    type="file"
+                    className="hidden"
+                    accept="audio/*"
+                    onChange={handleAudioChange}
+                    multiple
+                  />
+                </>
+              )}
+              {audioFile && (
+                <audio
+                  ref={audioRef}
+                  src={URL.createObjectURL(audioFile)}
+                  controls
+                >
+                  Your browser does not support the audio element.
+                </audio>
+              )}
+            </label>
+          </div>
+          {!isSameFile && (
+            <div className="flex items-center justify-center w-1/2">
+              <label
+                htmlFor="dropzone-file"
+                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                />
-              </svg>
-              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                <span className="font-semibold">Click to upload</span> or drag
-                and drop
-              </p>
-              <p className="text-xs text-gray-500 dark:text-gray-400">
-                Choose any audio file.
-              </p>
-            </div>
-            <input
-              id="dropzone-file"
-              type="file"
-              className="hidden"
-              accept="audio/*"
-              onChange={handleAudioChange}
-              multiple
-            />
-          </label>
-        </div>
-        <div className="flex items-center justify-center w-1/2">
-          <label
-            htmlFor="dropzone-file"
-            className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-          >
-            {!uploaded && (
-              <>
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <svg
-                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                <p className="mb-2 text-xl font-bold text-gray-500">Noise</p>
+                {!uploadedNoise && (
+                  <>
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                      <svg
+                        className="w-8 h-8 mb-4 text-gray-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 16"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                        />
+                      </svg>
+                      <p className="mb-2 text-sm text-gray-500">
+                        <span className="font-semibold">Click to upload</span>{" "}
+                        or drag and drop
+                      </p>
+                      <p className="text-xs text-gray-500">
+                        Choose any audio file.
+                      </p>
+                    </div>
+                    <input
+                      id="dropzone-file"
+                      type="file"
+                      className="hidden"
+                      accept="audio/*"
+                      onChange={handleNoiseChange}
+                      multiple
                     />
-                  </svg>
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    <span className="font-semibold">Click to upload</span> or
-                    drag and drop
-                  </p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">
-                    Choose any audio file.
-                  </p>
-                </div>
-                <input
-                  id="dropzone-file"
-                  type="file"
-                  className="hidden"
-                  accept="audio/*"
-                  onChange={handleAudioChange}
-                  multiple
-                />
-              </>
-            )}
-            {audioFile && (
-              <audio
-                ref={audioRef}
-                src={URL.createObjectURL(audioFile)}
-                controls
-              >
-                Your browser does not support the audio element.
-              </audio>
-            )}
-          </label>
+                  </>
+                )}
+                {noiseFile && (
+                  <audio
+                    ref={noiseRef}
+                    src={URL.createObjectURL(noiseFile)}
+                    controls
+                  >
+                    Your browser does not support the audio element.
+                  </audio>
+                )}
+              </label>
+            </div>
+          )}
         </div>
       </div>
     </div>
